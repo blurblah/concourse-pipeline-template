01319293dc113c0c261e6fecb24838e5aa405950
blurblah <blurblah@blurblah.net>
2018-10-15 16:57:39 +0900

    Fix regex for finding color in pipeline and change base docker image resource

--- ci/tasks/scripts/update-deploy-color.sh
+++ ci/tasks/scripts/update-deploy-color.sh
@@ -5,13 +5,12 @@ set -xe
 cd pipeline-repo/bin
 mv fly_linux_amd64 fly
 chmod +x fly
-#./fly -t local login -c ${CONCOURSE_URL} -n ${CONCOURSE_TEAM} -u ${CONCOURSE_USERNAME} -p ${CONCOURSE_PASSWORD}
 ./fly -t local login -c ${CONCOURSE_URL} -u ${CONCOURSE_USERNAME} -p ${CONCOURSE_PASSWORD}
 ./fly -t local sync
-./fly -t local get-pipeline -p ${CONCOURSE_PIPELINE} > original_pipeline.yml
+./fly -t local get-pipeline -p ${CONCOURSE_PIPELINE} > original-pipeline.yml
 
 # TODO: Decide color
-COLOR=$(cat original-pipeline.yml | grep '\- name: Deploy-*' | cut -d':' -f2 | cut -d'-' -f2)
+COLOR=$(cat original_pipeline.yml | grep '\- name: Deploy-*' | cut -d':' -f2 | cut -d'-' -f2)
 if [ "$COLOR" == "blue" ]
 then
     NEXT_COLOR="green"
@@ -20,7 +19,7 @@ else
 fi
 # End of the temporary color decision routine
 
-sed "s/Deploy-*/Deploy-${NEXT_COLOR}/" original_pipeline.yml > updated_pipeline.yml
-cat updated_pipeline.yml
+sed "s/Deploy-.*/Deploy-${NEXT_COLOR}/" original-pipeline.yml > updated-pipeline.yml
+cat updated-pipeline.yml
 
-./fly -t local set-pipeline -p ${CONCOURSE_PIPELINE} -c updated_pipeline.yml -n
+./fly -t local set-pipeline -p ${CONCOURSE_PIPELINE} -c updated-pipeline.yml -n
