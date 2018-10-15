4ba9c267a5c126c5ae8437b2410016de2c2d7c8d
blurblah <blurblah@blurblah.net>
2018-10-15 16:49:23 +0900

    Add params of update color task

--- ci/tasks/scripts/update-deploy-color.sh
+++ ci/tasks/scripts/update-deploy-color.sh
@@ -5,7 +5,8 @@ set -xe
 cd pipeline-repo/bin
 mv fly_linux_amd64 fly
 chmod +x fly
-./fly -t local login -c ${CONCOURSE_URL} -n ${CONCOURSE_TEAM} -u ${CONCOURSE_USERNAME} -p ${CONCOURSE_PASSWORD}
+#./fly -t local login -c ${CONCOURSE_URL} -n ${CONCOURSE_TEAM} -u ${CONCOURSE_USERNAME} -p ${CONCOURSE_PASSWORD}
+./fly -t local login -c ${CONCOURSE_URL} -u ${CONCOURSE_USERNAME} -p ${CONCOURSE_PASSWORD}
 ./fly -t local sync
 ./fly -t local get-pipeline -p ${CONCOURSE_PIPELINE} > original_pipeline.yml
 
