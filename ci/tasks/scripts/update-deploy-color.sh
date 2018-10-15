a49c27174c809c648d6af51f6ca72c53b6626273
blurblah <blurblah@blurblah.net>
2018-10-15 16:38:34 +0900

    Add test code

--- ci/tasks/scripts/update-deploy-color.sh
+++ ci/tasks/scripts/update-deploy-color.sh
@@ -4,6 +4,7 @@ set -xe
 
 cd pipeline-repo/bin
 chmod +x fly
+ls -al fly
 ./fly -t local login -c ${CONCOURSE_URL} -n ${CONCOURSE_TEAM} -u ${CONCOURSE_USERNAME} -p ${CONCOURSE_PASSWORD}
 ./fly -t local sync
 ./fly -t local get-pipeline -p ${CONCOURSE_PIPELINE} > original_pipeline.yml
