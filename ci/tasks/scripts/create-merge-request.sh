51ae30d5b38e22435c26a9aef71feda01d8a1a0f
blurblah <blurblah@blurblah.net>
2018-10-22 15:05:03 +0900

    Create merge request task

---
+++ ci/tasks/scripts/create-merge-request.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -xe
+
+cd pipeline-repo/bin
+tar xvfz lab_*.tar.gz
+
+mkdir -p ~/.config
+lab_config=~/.config/lab.hcl
+echo "\"core\" = {" > $lab_config
+echo "  \"host\" = \"$GITLAB_HOST\"" >> $lab_config
+echo "  \"token\" = \"$GITLAB_TOKEN\"" >> $lab_config
+echo "  \"user\" = \"$GITLAB_USERNAME\"" >> $lab_config
+echo "}" >> $lab_config
+cat $lab_config
+
+version=$(cat ../release-candidate-dev/version.txt)
+cd ../../source-repo
+../pipeline-repo/bin/lab mr create origin $PROD_BRANCH -m "Promote and deploy release $version"
