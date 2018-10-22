3bf0d8602c3612a6b50ed68dd0e0a64986d66709
blurblah <blurblah@blurblah.net>
2018-10-22 12:31:59 +0900

    Change app name of manifest.yml

--- ci/tasks/scripts/prepare-manifest.sh
+++ ci/tasks/scripts/prepare-manifest.sh
@@ -6,5 +6,7 @@ cd output
 release_candidate=$(cat ../release-candidate-dev/release_candidate.txt)
 cp -f ../release-candidate-dev/$release_candidate ./app.jar
 
-sed "s/path:.*/path: .\/app.jar/" ../source-repo/manifest.yml > manifest.yml
+app_name=$(cat ../source-repo/manifest.yml | grep "name:" | awk -F" " '{print $NF}')
+sed "s/name:.*/name: $app_name-dev/" ../source-repo/manifest.yml > manifest.yml
+sed -i "s/path:.*/path: .\/app.jar/" manifest.yml
 cat manifest.yml
