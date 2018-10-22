91fc18046f6f4d26fe8883ff28f06efe1dbcd29f
blurblah <blurblah@blurblah.net>
2018-10-22 11:57:56 +0900

    Update deploy job

--- ci/tasks/scripts/prepare-manifest.sh
+++ ci/tasks/scripts/prepare-manifest.sh
@@ -3,9 +3,8 @@
 set -xe
 
 cd output
-release_candidate=$(cat ../out-repo/release_candidate.txt)
-cp -f ../out-repo/$release_candidate ./app.jar
+release_candidate=$(cat ../release-candidate-dev/release_candidate.txt)
+cp -f ../release-candidate-dev/$release_candidate ./app.jar
 
-color=$(cat ../color-info/next_color.txt)
-sed "s/APPNAME/$PWS_APP_HOSTNAME\-$color/" ../pipeline-repo/manifests/manifest.yml > manifest.yml
+sed "s/path:.*/path: .\/app.jar/" ../source-repo/manifest.yml > manifest.yml
 cat manifest.yml
