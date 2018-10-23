16cc5ae0d08f337c4f33b6faa6326480e517cb03
blurblah <blurblah@blurblah.net>
2018-10-23 13:48:15 +0900

    Update rollback

--- ci/tasks/scripts/rollback-routes.sh
+++ ci/tasks/scripts/rollback-routes.sh
@@ -5,18 +5,24 @@ set -xe
 cf api $PWS_API --skip-ssl-validation
 cf login -u $PWS_USER -p $PWS_PWD -o "$PWS_ORG" -s "$PWS_SPACE"
 
-color=`cf apps | grep "$PWS_APP_HOSTNAME.$PWS_APP_DOMAIN" | cut -d' ' -f1 | awk -F"-" '{print $NF}'`
-if [ "$color" == "blue" ]
+curr_color=`cf apps | grep "$PWS_APP_HOSTNAME.$PWS_APP_DOMAIN" | cut -d' ' -f1 | awk -F"-" '{print $NF}'`
+if [ "$curr_color" == "blue" ]
 then
-    priv_color="green"
+    prev_color="green"
 else
-    priv_color="blue"
+    prev_color="blue"
 fi
 
-echo "Mapping main app route to point to $PWS_APP_HOSTNAME-$next_color instance"
-cf map-route "$PWS_APP_HOSTNAME-$next_color" $PWS_APP_DOMAIN --hostname $PWS_APP_HOSTNAME
+echo "Mapping previous app route to point to $PWS_APP_HOSTNAME-$next_color instance"
+is_exist_previous_app=`cf apps | grep "$PWS_APP_HOSTNAME\-$prev_color" | wc -l || true`
+if [ $is_exist_previous_app -ne 0 ]
+then
+    echo "$PWS_APP_HOSTNAME-$prev_color exists!"
+    cf map-route "$PWS_APP_HOSTNAME-$prev_color" $PWS_APP_DOMAIN --hostname $PWS_APP_HOSTNAME
+fi
+cf routes
 
-echo "Removing current service app route that pointed to $PWS_APP_HOSTNAME-$current_color instance"
+echo "Removing current service app route that pointed to $PWS_APP_HOSTNAME-$curr_color instance"
 is_exist_current_app=`cf apps | grep "$PWS_APP_HOSTNAME.$PWS_APP_DOMAIN" | grep "$PWS_APP_HOSTNAME\-$curr_color" | wc -l || true`
 if [ $is_exist_current_app -ne 0 ]
 then
--- ci/tasks/scripts/update-routes.sh
+++ ci/tasks/scripts/update-routes.sh
@@ -16,7 +16,7 @@ fi
 echo "Mapping main app route to point to $PWS_APP_HOSTNAME-$next_color instance"
 cf map-route "$PWS_APP_HOSTNAME-$next_color" $PWS_APP_DOMAIN --hostname $PWS_APP_HOSTNAME
 
-echo "Removing current service app route that pointed to $PWS_APP_HOSTNAME-$current_color instance"
+echo "Removing current service app route that pointed to $PWS_APP_HOSTNAME-$curr_color instance"
 is_exist_current_app=`cf apps | grep "$PWS_APP_HOSTNAME.$PWS_APP_DOMAIN" | grep "$PWS_APP_HOSTNAME\-$curr_color" | wc -l || true`
 if [ $is_exist_current_app -ne 0 ]
 then
