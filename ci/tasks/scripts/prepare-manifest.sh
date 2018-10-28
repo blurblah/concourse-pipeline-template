5efeb0922f03649b0cdca9cb2d43dc7227e04eb6
blurblah <blurblah@blurblah.net>
2018-10-28 17:24:07 +0900

    Use PWS_SPACE when modify manifest

--- ci/tasks/scripts/prepare-manifest.sh
+++ ci/tasks/scripts/prepare-manifest.sh
@@ -7,6 +7,6 @@ release_candidate=$(cat ../release-candidate-dev/release_candidate.txt)
 cp -f ../release-candidate-dev/$release_candidate ./app.jar
 
 app_name=$(cat ../source-repo/manifest.yml | grep "name:" | awk -F" " '{print $NF}')
-sed "s/name:.*/name: $app_name-dev/" ../source-repo/manifest.yml > manifest.yml
+sed "s/name:.*/name: $app_name-$PWS_SPACE/" ../source-repo/manifest.yml > manifest.yml
 sed -i "s/path:.*/path: .\/app.jar/" manifest.yml
 cat manifest.yml
